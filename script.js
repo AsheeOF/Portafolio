da5534911f28600d350e12309d00365833de490c
Alejandro <sogaale201@gmail.com>
2023-03-01 21:02:16 -0600

    frist commit

---
+++ script.js
@@ -0,0 +1,100 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+//formulario
+
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+  
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
+    }
+  }
+  
+  const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
+  
+  const mensajesDeError = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+      valueMissing: "El campo correo no puede estar vacío",
+      typeMismatch: "El correo no es válido",
+    },
+    
+    numero: {
+      valueMissing: "Este campo no puede estar vacío",
+      patternMismatch: "El formato requerido es XXXXXXXXXX 10 números",
+    },
+
+    texto: {
+      valueMissing: "Este campo no puede estar vacío",
+      patternMismatch: "El formato requerido es de 40 caracteres",
+    },
+
+  };
+   function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        console.log(tipoDeInput, error);
+        console.log(input.validity[error]);
+        console.log(mensajesDeError[tipoDeInput][error]);
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+  }
+  
