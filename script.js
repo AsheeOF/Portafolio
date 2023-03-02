416bc3e0e2defec1e9f47f8bb126cee31af4445c
Alejandro <sogaale201@gmail.com>
2023-03-02 11:17:37 -0600

    bugs check

--- script.js
+++ script.js
@@ -42,59 +42,24 @@ window.onscroll = function(){
 
 //formulario
 
-export function valida(input) {
-    const tipoDeInput = input.dataset.tipo;
-    if (validadores[tipoDeInput]) {
-      validadores[tipoDeInput](input);
-    }
-  
-    if (input.validity.valid) {
-      input.parentElement.classList.remove("input-container--invalid");
-      input.parentElement.querySelector(".input-message-error").innerHTML = "";
-    } else {
-      input.parentElement.classList.add("input-container--invalid");
-      input.parentElement.querySelector(".input-message-error").innerHTML =
-        mostrarMensajeDeError(tipoDeInput, input);
-    }
-  }
-  
-  const tipoDeErrores = [
-    "valueMissing",
-    "typeMismatch",
-    "patternMismatch",
-    "customError",
-  ];
-  
-  const mensajesDeError = {
-    nombre: {
-      valueMissing: "El campo nombre no puede estar vacío",
-    },
-    email: {
-      valueMissing: "El campo correo no puede estar vacío",
-      typeMismatch: "El correo no es válido",
-    },
-    
-    numero: {
-      valueMissing: "Este campo no puede estar vacío",
-      patternMismatch: "El formato requerido es XXXXXXXXXX 10 números",
-    },
+function submitForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const tema = document.getElementById("tema").value;
+  const mensaje = document.getElementById("mensaje").value;
 
-    texto: {
-      valueMissing: "Este campo no puede estar vacío",
-      patternMismatch: "El formato requerido es de 40 caracteres",
-    },
-
-  };
-   function mostrarMensajeDeError(tipoDeInput, input) {
-    let mensaje = "";
-    tipoDeErrores.forEach((error) => {
-      if (input.validity[error]) {
-        console.log(tipoDeInput, error);
-        console.log(input.validity[error]);
-        console.log(mensajesDeError[tipoDeInput][error]);
-        mensaje = mensajesDeError[tipoDeInput][error];
-      }
-    });
-    return mensaje;
+  if (name == "" || email == "" || phone == "") {
+    alert("Please fill out all mandatory fields!");
+  } else {
+    const formData = {
+      name: name,
+      email: email,
+      phone: phone,
+      tema: tema,
+      mensaje: mensaje
+    };
+    // send formData to server using fetch or AJAX
+    console.log(formData);
   }
-  
+}
